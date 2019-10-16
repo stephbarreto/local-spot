6b6721e311db8d8ab38a787c004dc24a32e5aac5
Stephanny Barreto <sdsb.snf@uea.edu.br>
2019-10-16 15:40:19 -0400

    atualizando back

--- src/js/presentation.js
+++ src/js/presentation.js
@@ -1,4 +1,4 @@
-import { Card, Button, InfoSection, FormGroupInput, Checkbox, Collapse, CollapseItem, Slider } from '@/components';
+import { Card, Button, InfoSection, FormGroupInput, Checkbox, Collapse, CollapseItem, Slider, Modal } from '@/components';
 import { Carousel, CarouselItem, Tooltip } from 'element-ui'
 import axios from 'axios';
 import Router from "vue-router";
@@ -9,6 +9,7 @@ export default {
   bodyClass: 'ecommerce-page',
   components: {
     Card,
+    Modal,
     InfoSection,
     Collapse,
     CollapseItem,
@@ -22,9 +23,32 @@ export default {
   },
   data() {
     return {
+      
+      modals: {
+        login: false, 
+        reserva: false, 
+        cadastro_vaga: false,
+        cadastro_user: false
+      },
+      form: {
+        user_id: '',
+        password: ''
+      },
+      cadastro: {
+        id: '',
+        nome: '',
+        saldo: ''
+      },
+      cadastro_vaga: {
+        id: '',
+        endereco: '',
+        valor: '',
+      
+      },
+      dados_vaga: [],
+      dados: [],
+      motorista: [],
       vagas: [],
-      endereco: '',
-      valor: '',
       errors: [],
       filters: {
         priceRange: [2, 10],
@@ -52,26 +76,79 @@ export default {
     }
   },
   created() {
-        axios.get('http://localhost:3000/api/Vaga')
-        .then(response => {
-        this.endereco = response.data.information
-        this.valor = response.data.valor
-        console.log(response.data);
+      // axios.get('http://localhost:3000/api/org.spotale.model.Motorista')
+      // .then(response => {
+      // this.motoristas = response.data
+      // this.valor = response.data.valor
+      // console.log(response.data);
 
-        })
-        .catch(e => {
-        this.errors.push(e)
-        })
-        axios.get('http://localhost:3000/api/Vaga')
-        .then(response => {
-        this.vagas = response.data
-        console.log(response.data);
+      // })
+      // .catch(e => {
+      // this.errors.push(e)
+      // })
+      axios.get('http://localhost:3000/api/org.spotale.model.Vaga')
+      .then(response => {
+      this.vagas = response.data
+      // console.log(response.data);
 
-        })
-        .catch(e => {
-        this.errors.push(e)
-        })
+      })
+      .catch(e => {
+      this.errors.push(e)
+      })
     }, 
+    methods: {
+      entrar: function(event){
+        var form = { 
+          user_id: this.form.user_id,
+          password: this.form.vaga_id
+        }
+        console.log(form);
+        axios.get('http://localhost:3000/api/org.spotale.model.Motorista/'+form.user_id)
+        .then(response => {
+        this.motorista = response.data
+        console.log(response.data);
+        this.id_motorista = response.data.motoristaId
+        })
+        .catch(e => {
+        this.errors.push(e)
+        })
+        axios.get('http://localhost:3000/api/org.spotale.model.Vaga/'+form.password)
+        .then(response => {
+        this.dados_vaga = response.data
+        console.log(response.data);
+        this.id_vaga = response.data.vagaId
+        })
+        .catch(e => {
+        this.errors.push(e)
+        })
+      }, 
+      cadastrar: function (event) {
+        axios.post('http://localhost:3000/api/org.spotale.model.Motorista', {
+          motoristaId: this.cadastro.id,
+          nome: this.cadastro.nome, 
+          saldo: this.cadastro.saldo
+        })
+      },
+      cadastrar_vaga: function (event) {
+        axios.post('http://localhost:3000/api/org.spotale.model.Vaga', {
+          vagaId: this.cadastro_vaga.id,
+          locatario: 'org.spotale.model.Motorista#admin', 
+          endereco: this.cadastro_vaga.endereco,
+          valor: this.cadastro_vaga.valor,
+          disponivel: 'true',
+          paraAluguel: 'true'
+        })
+      }, 
+      realizar_reserva: function (event) {
+        axios.post('http://localhost:3000/api/org.spotale.model.CheckIn', {
+          motorista: 'org.spotale.model.Motorista#'+this.id_motorista,
+          vaga: 'org.spotale.model.Vaga#'+this.id_vaga,
+          timestamp: Date.now(),
+          transactionId: ''
+        })
+      }
+
+  }
 
 
 }
--- src/pages/Presentation.vue
+++ src/pages/Presentation.vue
@@ -36,6 +36,15 @@
             <div class="section">
                 <div class="container">
                     <h2 class="section-title">Categorias</h2>
+                    <div class="justify-content-right">
+                        <n-button type="default" @click.native="modals.cadastro_vaga = true">
+                            Cadastrar Vaga
+                        </n-button>
+                        <n-button type="default" @click.native="modals.cadastro_user = true">
+                            Cadastar Usuário
+                        </n-button>
+                    </div>
+                    
                     <div class="row">
                         <div class="col-md-3">
                             <div class="collapse-panel">
@@ -91,65 +100,195 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- <div class="col-md-9">
-                            <div class="row">
-                                <div class="col-lg-4 col-md-6">
-                                    <card type="vaga" plain>
-                                        <img slot="image" src="img/vaga1.jpg" alt="">
-                                        <a href="#">
-                                            <h4 class="card-title">{{endereco}}</h4>
-                                        </a>
-                                        <p class="card-description">
-                                            {{valor}}
-                                        </p>
-                                        <div class="card-footer">
-                                            <div class="price-container">
-                                                <span class="price"> {{valor}} </span>
-                                            </div>
-                                        
-                                            <el-tooltip effect="light" content="Remove from wishlist" placement="top">
-                                                <n-button type="danger" icon round class="pull-right btn-neutral">
-                                                    <i class="now-ui-icons ui-2_favourite-28"></i>
-                                                </n-button>
-                                            </el-tooltip>
-                                        </div>
-                                    </card>
-                                </div> -->
+                        
 
-                                <div class="col-lg-4 col-md-6"
-                                     v-for="vaga in vagas"
-                                     :key="vaga.endereco">
-                                    <card type="vaga" plain>
-                                        <img slot="image" :src="products.image" alt="">
+                        <div class="col-lg-4 col-md-6"
+                                v-for="vaga in vagas"
+                                :key="vaga.endereco">
+                            <card type="vaga" plain>
+                                <img slot="image" :src="products.image" alt="">
 
-                                        <a href="#">
-                                            <h4 class="card-title">{{vaga.endereco}}</h4>
-                                        </a>
-                                        <p class="card-description">
-                                            {{vaga.disponivel}}
-                                        </p>
-                                        <div class="card-footer">
-                                            <div class="price-container">
-                                                <span class="price"> R$ {{vaga.valor}}</span>
-                                            </div>
-                                            <el-tooltip effect="light" content="Remove from wishlist" placement="top">
-                                                <n-button type="danger" icon round class="pull-right btn-neutral">
-                                                    <i class="now-ui-icons ui-2_favourite-28"></i>
-                                                </n-button>
-                                            </el-tooltip>
-                                        </div>
-                                        <button class="btn btn-primary round" data-toggle="modal" @click.native="showSwal">
-                                            Reservar
-                                        </button>
-
-                                    </card>
-                                </div>
+                                <a href="#">
+                                    <h4 class="card-title">{{vaga.endereco}}</h4>
+                                </a>
+                                <p class="card-description">
+                                    {{vaga.disponivel}}
+                                </p>
                                 
-                            </div>
-                        <!-- </div> -->
+                                <div class="card-footer">
+                                    <div class="price-container">
+                                        <span class="price"> R$ {{vaga.valor}}</span>
+                                    </div>
+                                    
+                                    <el-tooltip effect="light" content="Remove from wishlist" placement="top">
+                                        <n-button type="danger" icon round class="pull-right btn-neutral">
+                                            <i class="now-ui-icons ui-2_favourite-28"></i>
+                                        </n-button>
+                                    </el-tooltip>
+                                </div>
+                                <n-button type="primary" @click.native="modals.login = true">
+                                    Reservar
+                                </n-button>
+
+                            </card>
+                        </div>
+                                
+                        </div>
                     </div>
                 </div>
             </div>
+            <modal :show.sync="modals.login"
+               modal-classes="modal-login"
+               class="modal-primary">
+            <div class="card card-plain card-login" data-background-color="" slot="base-content">
+                <div class="modal-header justify-content-center">
+                    <button type="button" class="close" @click.native="modals.login = false" aria-hidden="true">
+                        <i class="now-ui-icons ui-1_simple-remove"></i>
+                    </button>
+                    <div class="header header-primary text-center">
+                        <div class="logo-container">
+                            <img v-lazy="'img/now-logo.png'" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <form class="form" method="" action="">
+                        <div class="card-body">
+                            <fg-input class="no-border input-lg"
+                                      addon-left-icon="now-ui-icons users_circle-08"
+                                      v-model="form.user_id"
+                                      placeholder="User ID">
+                            </fg-input>
+
+                            <fg-input class="no-border input-lg"
+                                      type="password"
+                                      addon-left-icon="now-ui-icons ui-1_lock-circle-open"
+                                      v-model="form.vaga_id"
+                                      placeholder="Senha...">
+                            </fg-input>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <button type="submit" class="btn btn-neutral btn-round btn-lg btn-block" @click="modals.reserva = true" v-on:click="entrar">Efetuar Reserva</button>
+                </div>
+            </div>
+        </modal>
+
+        <modal :show.sync="modals.reserva"
+               modal-classes="modal-reserva"
+               class="modal-primary">
+            <div class="card card-plain card-reserva" data-background-color="" slot="base-content">
+                <div class="modal-header justify-content-center">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                        <i class="now-ui-icons ui-1_simple-remove"></i>
+                    </button>
+                    <div class="header text-center">
+                        Efetuar Reserva
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <h5 class="text-center">Informações do Motorista</h5>
+                    <hr>
+                    <p><strong>Identificação:</strong> {{motorista.motoristaId}}</p>
+                    <hr>
+                    <p><strong>Nome:</strong> {{motorista.nome}}</p>
+                    <hr>
+                    <p><strong>Saldo:</strong> {{motorista.saldo}}</p>
+                    <p></p>
+                    <hr>
+                    <p></p>
+                    <h5 class="text-center">Dados da Vaga</h5>
+                    <p><strong>Endereço:</strong>{{dados_vaga.endereco}}</p>
+                    <p><strong>Valor:</strong>{{dados_vaga.valor}}</p>
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <button type="button" class="btn btn-default btn-round" v-on:click="realizar_reserva" @click.native="modals.reserva = false">Vaaaaai!</button>
+                </div>
+                </div>
+        </modal>
+        <modal :show.sync="modals.cadastro_user"
+               modal-classes="modal-reserva"
+               class="modal-primary">
+            <div class="card card-plain card-login" data-background-color="" slot="base-content">
+                <div class="modal-header justify-content-center">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                        <i class="now-ui-icons ui-1_simple-remove"></i>
+                    </button>
+                </div>
+                <div class="header text-center">
+                    Cadastrar Usuário
+                </div>
+                <div class="modal-body">
+                    <form class="form" method="" action="">
+                        <div class="card-body">
+                            <fg-input class="no-border input-lg"
+                                      addon-left-icon="now-ui-icons users_circle-08"
+                                      v-model="cadastro.id"
+                                      placeholder="Identificação">
+                            </fg-input>
+                            <fg-input class="no-border input-lg"
+                                      addon-left-icon="now-ui-icons users_circle-08"
+                                      v-model="cadastro.nome"
+                                      placeholder="Nome">
+                            </fg-input>
+                            <fg-input class="no-border input-lg"
+                                      addon-left-icon="now-ui-icons users_circle-08"
+                                      v-model="cadastro.saldo"
+                                      placeholder="Saldo">
+                            </fg-input>
+                            <fg-input class="no-border input-lg"
+                                      type="password"
+                                      addon-left-icon="now-ui-icons ui-1_lock-circle-open"
+                                      v-model="form.password"
+                                      placeholder="Senha...">
+                            </fg-input>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <button type="button" class="btn btn-default btn-round" data-dismiss="modal" @click.native="modals.cadastro_user = false" v-on:click="cadastrar">Cadastrar!</button>
+                </div>
+            </div>
+        </modal>
+        <modal :show.sync="modals.cadastro_vaga"
+               modal-classes="modal-reserva"
+               class="modal-primary">
+            <div class="card card-plain card-reserva" data-background-color="" slot="base-content">
+                <div class="modal-header justify-content-center">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                        <i class="now-ui-icons ui-1_simple-remove"></i>
+                    </button>
+                    <div class="header text-center">
+                        Efetuar Reserva
+                    </div>
+                </div>
+                <div class="modal-body">
+                    <form class="form" method="" action="">
+                        <div class="card-body">
+                            <fg-input class="no-border input-lg"
+                                      addon-left-icon="now-ui-icons users_circle-08"
+                                      v-model="cadastro_vaga.id"
+                                      placeholder="Identificação">
+                            </fg-input>
+                            <fg-input class="no-border input-lg"
+                                      addon-left-icon="now-ui-icons users_circle-08"
+                                      v-model="cadastro_vaga.endereco"
+                                      placeholder="Nome">
+                            </fg-input>
+                            <fg-input class="no-border input-lg"
+                                      addon-left-icon="now-ui-icons users_circle-08"
+                                      v-model="cadastro_vaga.valor"
+                                      placeholder="Valor">
+                            </fg-input>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer justify-content-center">
+                    <button type="button" class="btn btn-default btn-round" data-dismiss="modal" v-on:click="cadastrar_vaga">Cadastrar!</button>
+                </div>
+                </div>
+        </modal>
         </div>
        
     
